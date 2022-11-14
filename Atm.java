90a1dfc4dfa57469fa90aceac1091fdd97a6da6f
Arun <arun35476@gmail.com>
2022-11-14 10:56:27 +0530

    Exception Handling

---
+++ Atm.java
@@ -0,0 +1,50 @@
+package exception.handling;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class InsufficientMoneyException extends Exception {
+
+	InsufficientMoneyException() {
+	}
+
+}
+
+public class Atm {
+
+	public static void main(String[] args) {
+
+		int totalBalance = 2000;
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("TotalBalance " + totalBalance);
+
+		System.out.print("Enter amount to Withdraw : ");
+
+		int withDraw = 0;
+		try {
+
+			withDraw = scanner.nextInt();
+			if (withDraw > totalBalance)
+				throw new InsufficientMoneyException();
+
+		} catch (InsufficientMoneyException exception) {
+		//	System.out.println("Insufficient Balance");
+			exception.printStackTrace();
+		} catch (InputMismatchException exception) {
+			System.out.println("Enter Number Only ");
+			exception.printStackTrace();
+		}finally {
+			if (withDraw < totalBalance && withDraw > 0) {
+				System.out.println("You Withdrawed " + withDraw + " Rupees");
+			} else {
+				System.out.println("Available Amount " + totalBalance);
+			}
+		}
+		
+	      System.out.println("VISIT AGAIN");
+
+	}
+
+}
